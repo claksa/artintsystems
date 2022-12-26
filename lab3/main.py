440ad9c78d5b65aabfda0f6bf9e8e1f41e0076b2
ppolya <polina.pavlova19.10@gmail.com>
2022-12-26 10:54:13 +0300

    tried find useful freq-s

--- lab3/main.py
+++ lab3/main.py
@@ -1,11 +1,12 @@
 import math
 import random
 
+# T - data, i - column, c - param
+def freq(T, column, p):
+    return T[column].count(p)
+
 data = []
-all_data = []
 row = []
-next_list = list(range(6))
-random.shuffle(next_list)
 with open("DATA.csv", newline='') as file:
     for line in file:
         row = line.split(';')
@@ -18,9 +19,23 @@ attr_num = math.sqrt(len(row)+1)
 random_attr = list(range(round(attr_num)))
 random.shuffle(random_attr)
 dataset = [([data[k][j] for j in random_attr], data[k][-1]) for k in range(len(data))]
-print(dataset)
 
+classes = []
+for i in range(len(random_attr)):
+    c = set()
+    for row in dataset:
+        params = row[0]
+        c.add(params[i])
+    classes.append(c)
 
+dict = dict()
+for i in random_attr:
+    attr = []
+    for row in dataset:
+        params = row[0]
+        attr.append(params[i])
+    dict[i] = attr
 
-
-
+freqs = []
+for k, v in dict.items():
+    print(v.count(1))
