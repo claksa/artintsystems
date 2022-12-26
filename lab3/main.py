658ee7dc0b3abe5b685b890c44fea2856e0628fa
ppolya <polina.pavlova19.10@gmail.com>
2022-12-26 07:32:40 +0300

    readed csv file

--- lab3/main.py
+++ lab3/main.py
@@ -1,1 +1,26 @@
+import math
+import random
+
+data = []
+all_data = []
+row = []
+next_list = list(range(6))
+random.shuffle(next_list)
+with open("DATA.csv", newline='') as file:
+    for line in file:
+        row = line.split(';')
+        for i in range(1,len(row)):
+            row[i] = int(row[i])
+        row.pop(0)
+        data.append(row)
+
+attr_num = math.sqrt(len(row)+1)
+random_attr = list(range(round(attr_num)))
+random.shuffle(random_attr)
+dataset = [([data[k][j] for j in random_attr], data[k][-1]) for k in range(len(data))]
+print(dataset)
+
+
+
+
 
