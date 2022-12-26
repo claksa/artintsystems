82e9f4c7417f09b0b290fb5ec487d4e12866714c
ppolya <polina.pavlova19.10@gmail.com>
2022-12-26 12:31:07 +0300

    correct freq i think

--- lab3/main.py
+++ lab3/main.py
@@ -1,9 +1,8 @@
 import math
 import random
 
-# T - data, i - column, c - param
-def freq(T, column, p):
-    return T[column].count(p)
+def freq(T, p):
+    return T.count(p)
 
 data = []
 row = []
@@ -20,14 +19,6 @@ random_attr = list(range(round(attr_num)))
 random.shuffle(random_attr)
 dataset = [([data[k][j] for j in random_attr], data[k][-1]) for k in range(len(data))]
 
-classes = []
-for i in range(len(random_attr)):
-    c = set()
-    for row in dataset:
-        params = row[0]
-        c.add(params[i])
-    classes.append(c)
-
 dict = dict()
 for i in random_attr:
     attr = []
@@ -35,7 +26,26 @@ for i in random_attr:
         params = row[0]
         attr.append(params[i])
     dict[i] = attr
+print(dict.keys())
 
-freqs = []
-for k, v in dict.items():
-    print(v.count(1))
+# classes = []
+# for i in range(len(random_attr)):
+#     c = set()
+#     for row in dataset:
+#         params = row[1]
+#         c.add(params)
+#     classes.append(list(c))
+# print(classes)
+
+metrics = set()
+for row in dataset:
+    metrics.add(row[1])
+metrics = list(metrics)
+values = list(dict.values())
+all_freqs = []
+for m in metrics:
+    freqs = []
+    for v in values:
+        freqs.append(v.count(m))
+    all_freqs.append(freqs)
+print(all_freqs)
