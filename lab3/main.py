c37822ba132a83100c8c45843b99506af0422877
Полина Дмитриевна Павлова <312196@niuitmo.ru>
2023-02-08 22:48:25 +0300

    added some handlings

--- lab3/main.py
+++ lab3/main.py
@@ -1,5 +1,14 @@
 import math
 import random
+import numpy as np
+
+def print_dict(dict):
+    for key in dict:
+        print(key, '->', dict[key])
+
+def freq(T, p):
+    return T.count(p)
+
 
 data = []
 with open("DATA.csv", newline='') as file:
@@ -9,41 +18,69 @@ with open("DATA.csv", newline='') as file:
             row[i] = int(row[i])
         row.pop(0)
         data.append(row)
+attr_amount = round(math.sqrt(len(row)+1)) 
+attr_nums = random.sample(range(len(row)), attr_amount)
+print('indexes of random attributes: ', attr_nums)
+dataset = [([data[k][j] for j in attr_nums], data[k][-1]) for k in range(len(data))]
+dict_attr = dict(zip(attr_nums, [None]*len(attr_nums)))
 
-attr_num = math.sqrt(len(row)+1)
-attr_nums = random.sample(range(len(row)+1), round(attr_num))
-print(attr_nums)
-random_attr = list(range(round(attr_num)))
-print(random_attr)
-random.shuffle(random_attr)
-dataset = [([data[k][j] for j in random_attr], data[k][-1]) for k in range(len(data))]
-
-dict = dict()
-for i in random_attr:
-    attr = []
-    for row in dataset:
-        params = row[0]
-        attr.append(params[i])
-    dict[i] = attr
-print(dict.keys())
-
-# classes = []
-# for i in range(len(random_attr)):
-#     c = set()
-#     for row in dataset:
-#         params = row[1]
-#         c.add(params)
-#     classes.append(list(c))
-# print(classes)
-
-metrics = set()
+attributes = []
 for row in dataset:
-    metrics.add(row[1])
-metrics = list(metrics)
-values = list(dict.values())
-all_freqs = []
-for m in metrics:
-    freqs = []
-    for v in values:
-        freqs.append(v.count(m))
-    all_freqs.append(freqs)
+    attributes.append(row[0])
+attributes = np.array(attributes).transpose()
+for attr, key in zip(attributes, dict_attr):
+    dict_attr[key] = np.unique(attr, return_counts=True)
+print(dict_attr)
+
+grades = set()
+for row in dataset:
+    grades.add(row[1])
+grades = list(grades)
+
+dict_T = dict(zip(grades, []*len(grades)))
+for key in grades:
+    attributes = []
+    for row in dataset:
+        if row[1] == key:
+            attributes.append(row[0])
+    dict_T[key] = (np.array(attributes), len(attributes))
+
+
+info_ent = 0
+s = 0
+dataset_size = len(dataset)
+for key in dict_T:
+    freq = dict_T[key][1]
+    p = freq/dataset_size
+    s += p*math.log2(p)
+info_ent = -s
+
+# example dataset
+sample_arr = np.array([
+    [1, 1, 1, 0, 0],
+    [1, 1, 1, 1, 0],
+    [2, 1, 1, 0, 1]
+    ])
+transposed_sample_arr = sample_arr.transpose()
+classes = transposed_sample_arr[-1]
+print(classes)
+transposed_sample_arr = transposed_sample_arr[:-1, :]
+
+
+
+def info_x(labels):
+    s = 0
+    for label in labels:
+        label = np.array(label)
+        label = label.transpose()
+        print(label)
+        #info(label)
+
+labels = []
+for a in transposed_sample_arr:
+    labels.append(
+        [list(a), list(classes)]
+        )
+info_x(labels)
+
+
